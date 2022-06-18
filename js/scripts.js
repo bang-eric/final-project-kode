c07a183176a50c37c1e36cb02d203fa2ee8a03e2
bang-eric <ezraricardo@gmail.com>
2022-06-19 06:16:40 +0700

    add feature grayscale mode & normal mode with javascript DOM

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const gray = document.getElementById('grayButton');
+const normal = document.getElementById('normalButton');
+
+function grayScale(element, opacity) {
+    element.style.opacity = opacity;
+};
+
+function normalMode(element, opacity) {
+    element.style.opacity = opacity;
+};
